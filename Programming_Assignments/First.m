53c8e596da6c7727bc5abc3da44e01469309b3a6
tm1611 <tmeiendresch@gmx.de>
2019-03-15 11:34:31 +0100

    Initial commit

---
+++ Programming_Assignments/First.m
@@ -0,0 +1,8 @@
+% Initialize a 3 by 2 matrix
+A = [1,2;3,4;5,6]
+A
+
+% Initialize a 2 by 1 matrix 
+B = [1;2]
+
+A*B 
---
+++ Programming_Assignments/Intro.m
@@ -0,0 +1,14 @@
+cd 'C:/Users/TM/Dropbox/DS/Coursera_ML_Stanford/Programming_Assignments'
+
+% Intro to Octave/Matlab
+
+% Basic operators
+
+% Control Statements: for, while, if statement
+
+v = zeros
+
+for i = 1:10,
+v(i) = 2^i;
+end;
+v
